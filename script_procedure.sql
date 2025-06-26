1732495f4e09f03e31ed53fc14501db4a6aa4ada
Gustavo Henrique <gustavosousasantos54@gmail.com>
2025-06-26 17:19:41 -0300

    feat: carregando o procedure para repositorio

--- script_procedure.sql
+++ script_procedure.sql
@@ -40,29 +40,3 @@
 -- Testando
 CALL calcular_media_vendas_por_plataforma();
 
--- ----------------------------Criação e teste do Trigger---------------------------------------------
--- CREATE TABLE log_consertos(
---     id_log SERIAL PRIMARY KEY,
---     id_conserto INT,
---     data_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---     descricao TEXT
--- );
-
--- CREATE OR REPLACE FUNCTION registrar_log_conserto()
--- RETURNS TRIGGER
--- LANGUAGE plpgsql
--- AS $$
--- BEGIN
---     INSERT INTO log_consertos (id_conserto, descricao)
---     VALUES (NEW.id_conserto, 'Novo conserto registrado para o cliente ID ' || NEW.id_cliente);
---     RETURN NEW;
--- END;
--- $$
-
--- CREATE TRIGGER tg_log_consertos
--- AFTER INSERT ON fato_consertos
--- FOR EACH ROW
--- EXECUTE FUNCTION registrar_log_conserto();
-
--- Testando
-SELECT * FROM log_consertos;
